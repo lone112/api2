60e3df780667d2e900af5f409ebf0b2552d57202
fei.xiao <fei.xiao@yungoal.com>
2019-06-26 18:24:29 +0800

    response hostname

--- server.js
+++ server.js
@@ -1,36 +1,37 @@
-var restify = require('restify');
+var restify = require('restify')
+const os = require('os')
 
-function respond(req, res, next) {
-  res.send('HI ' + req.params.name);
-  next();
+function respond (req, res, next) {
+  res.send('HI ' + req.params.name + ' from ' + os.hostname())
+  next()
 }
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+function normalizePort (val) {
+  var port = parseInt(val, 10)
 
   if (isNaN(port)) {
     // named pipe
-    return val;
+    return val
   }
 
   if (port >= 0) {
     // port number
-    return port;
+    return port
   }
 
-  return false;
+  return false
 }
 
-var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+var server = restify.createServer()
+server.get('/hello/:name', respond)
+server.head('/hello/:name', respond)
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '3000')
 
-server.listen(port, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+server.listen(port, function () {
+  console.log('%s listening at %s', server.name, server.url)
+})
