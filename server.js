55af9d227f8e83ce3bf4be25ea79bae463fdaae8
fei.xiao <fei.xiao@yungoal.com>
2019-06-26 18:13:21 +0800

    add tests

--- server.js
+++ server.js
@@ -5,10 +5,32 @@ function respond(req, res, next) {
   next();
 }
 
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(80, function() {
+var port = normalizePort(process.env.PORT || '3000');
+
+server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
